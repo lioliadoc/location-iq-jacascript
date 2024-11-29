6de5bcfc5061484a16446017aac623c404f580be
lioliadoc <liolia.doc@gmail.com>
2024-11-28 19:59:28 -0600

    location-oq-complete

---
+++ location-iq.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const LOCATIONIQ_KEY = process.env['api_key'];
+console.log(LOCATIONIQ_KEY);
+
+const findLatitudeAndLongitude = (query) => {
+    let latitude, longitude;
+    axios.get('https://us1.locationiq.com/v1/search.php',
+    {
+        params: {
+            key: LOCATIONIQ_KEY,
+            q: query,
+            format: 'json'
+        }
+    })
+    .then((response) => {
+        latitude = response.data[0].lat;
+        longitude = response.data[0].lon;
+        console.log(latitude, longitude);
+    })
+    .catch ((error) => {
+        console.log('error');
+    });
+    return {
+        lat:latitude,
+        lon:longitude
+
+    }
+}
+
+const longLat = findLatitudeAndLongitude ('Christ the Redeemer')
